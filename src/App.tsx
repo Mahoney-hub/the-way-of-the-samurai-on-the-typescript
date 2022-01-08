f3bb9805707a4b603431f71fa01d17dc22817230
Pavel Vasilev <pv9998286620@yandex.ru>
2022-01-08 17:55:05 +0300

    lesson 20

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
     return (
-        <BrowserRouter>
             <div className={'app-wrapper'}>
                 <Header/>
                 <Navbar/>
@@ -19,7 +18,6 @@ const App = () => {
                     </Routes>
                 </div>
             </div>
-        </BrowserRouter>
     );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import classes from "./Navbar.module.css";
+import classes from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
+
+const setActive = ({isActive}:any) => isActive ? `${classes.active}` : '';
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.nav__item}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile" className={setActive}>Profile</NavLink>
             </div>
             <div className={classes.nav__item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs" className={setActive}>Messages</NavLink>
             </div>
             <div className={classes.nav__item}>
                 <a href="#s">News</a>
