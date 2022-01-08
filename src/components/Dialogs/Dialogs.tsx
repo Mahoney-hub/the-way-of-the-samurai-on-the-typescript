7cdc444c672e813f363ed7ad3fbe58fe879f6665
Pavel Vasilev <pv9998286620@yandex.ru>
2022-01-08 18:19:20 +0300

    lesson 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,31 @@ import classes from './Dialogs.module.css';
 const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
-            <h1>Dialogs</h1>
+            <div className={classes.dialogsItems}>
+                <div className={`${classes.dialog} ${classes.active}`}>
+                    Dimych
+                </div>
+                <div className={classes.dialog}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+                <div className={classes.dialog}>
+                    Sasha
+                </div>
+                <div className={classes.dialog}>
+                    Victor
+                </div>
+                <div className={classes.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How is your it-kamasutra</div>
+                <div className={classes.message}>Yo</div>
+            </div>
         </div>
     );
 };
